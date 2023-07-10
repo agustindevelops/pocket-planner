7b7fbaea6161d385a42cc8ffa9ccf6c0cc591006
Kaden <kadengraybill@gmail.com>
2023-07-10 11:09:22 -0500

    somestyling

--- components/Welcome/Welcome.styles.ts
+++ components/Welcome/Welcome.styles.ts
@@ -1,7 +1,9 @@
-import { createStyles } from '@mantine/core';
+import { Autocomplete, Grid, createStyles } from '@mantine/core';
 
 export default createStyles((theme) => ({
   root: {
+    width: '100%',
+    height: '100vh',
     backgroundColor: theme.colorScheme === 'dark' ? '#F7EBDB' : '#FFF8EF',
   },
   title: {
@@ -29,5 +31,29 @@ export default createStyles((theme) => ({
     fontFamily: 'Urbanist Regular',
     fontSize: 20,
     fontWeight: 300,
+  },
+  container: {
+    maxWidth: '1240px',
+    padding: '0 1rem',
+    margin: 'auto',
+  },
+  content: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+  },
+  col1: {
+    padding: '1rem 1rem 1rem 0',
+    height: '70vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  col2: {
+    padding: '1rem',
+    height: '70vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    fontSize: '.8rem',
   },
 }));
--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,5 +1,5 @@
 //stuff
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Anchor, Button } from '@mantine/core';
 import useStyles from './Welcome.styles';
 
 //components
@@ -9,9 +9,9 @@ export function Welcome() {
 
   return (
     <div className={classes.root}>
-      <div className="Container">
-        <div className="content">
-          <div className="col-1">
+      <div className={classes.container}>
+        <div className={classes.content}>
+          <div className={classes.col1}>
             <Title className={classes.title} align="center" mt={100}>
               The Best Way to{' '}
               <Text inherit variant="gradient" component="span">
@@ -35,12 +35,15 @@ export function Welcome() {
               . To get started edit index.tsx file.
             </Text>
           </div>
-          <div className="col-2">
+          <div className={classes.col2}>
             <div className="emailList">
               <div className="form-container">
                 <p className="sign-in-txt">
                   Join our Mailling List to get Notified When the App is Available
                 </p>
+                <a href="http://pocketplannerapp.com/" rel="noreferrer" target="_blank">
+                  <Button>Join</Button>
+                </a>
               </div>
             </div>
           </div>
